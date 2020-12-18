1ff5762508d83bee953112efe0664c6b33e56542
Elijah Arcedera <eli.arcedera@gmail.com>
2020-12-18 08:26:25 +0800

    added variables for each button

--- Calculator/calc.js
+++ Calculator/calc.js
@@ -1,3 +1,5 @@
+// Give each button a variable
+
 // Number buttons
 let oneBtn = document.getElementById("calc-one");
 let twoBtn = document.getElementById("calc-two");
@@ -9,3 +11,15 @@ let sevenBtn = document.getElementById("calc-seven");
 let eightBtn = document.getElementById("calc-eight");
 let nineBtn = document.getElementById("calc-nine");
 let zeroBtn = document.getElementById("calc-zero");
+
+// Other elements
+let decimalBtn = document.getElementById("calc-decimal");
+let clearBtn = document.getElementById("calc-clear");
+let backspaceBtn = document.getElementById("calc-backspace");
+let displayValBtn = document.getElementById("calc-display-val");
+
+// All the number buttons
+let calcNumBtns = document.getElementById("calc-btn-num");
+
+// All the operator buttons
+let calcOperatorBtns = document.getElementById("calc-btn-operator");
