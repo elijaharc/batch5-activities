4292a08c1ae2aa23fec05f9ff8222d92f9ef6b97
Elijah Arcedera <eli.arcedera@gmail.com>
2020-12-18 08:20:30 +0800

    initial commit

---
+++ Calculator/calc.js
@@ -0,0 +1,11 @@
+// Number buttons
+let oneBtn = document.getElementById("calc-one");
+let twoBtn = document.getElementById("calc-two");
+let threeBtn = document.getElementById("calc-three");
+let fourBtn = document.getElementById("calc-four");
+let fiveBtn = document.getElementById("calc-five");
+let sixBtn = document.getElementById("calc-six");
+let sevenBtn = document.getElementById("calc-seven");
+let eightBtn = document.getElementById("calc-eight");
+let nineBtn = document.getElementById("calc-nine");
+let zeroBtn = document.getElementById("calc-zero");
