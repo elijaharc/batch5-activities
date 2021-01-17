7a4296a2a31c6ac1b7d9ecee8759a1a30e3fc98b
Elijah Arcedera <eli.arcedera@gmail.com>
2021-01-17 14:39:50 +0800

    copied function

--- JavaScript Training Cave/app.js
+++ JavaScript Training Cave/app.js
@@ -5,7 +5,40 @@ let count = 0;
 //   count++;
 // }
 
-while (count < 3) {
-  count++;
-  console.log(count);
+// while (count < 3) {
+//   count++;
+//   console.log(count);
+// }
+
+function withdraw(user, amount) {
+  // Try to make the amount as accurate as possible (search for floating points)
+  // check if amount is a valid number, CANNOT BE NEGATIVE NUMBER
+  if (amount <= 0) {
+    let error = "Please enter valid amount.";
+    return error;
+  }
+
+  // Get user information from users array
+  if (userExist(user)) {
+    // Once user is found, add the amount to the balance AND RETURN new balance
+    for (let i = 0; i < users.length; i++) {
+      // console.log(users[i].user);
+      if (users[i].user.toUpperCase() == user.toUpperCase()) {
+        // check balance if enough to make a withdrawal
+        // console.log(users.toUpperCase().balance);
+        if (users[i].balance < amount) {
+          console.log("Sorry, balance insufficient");
+        } else {
+          users[i].balance -= amount;
+          console.log(
+            `New balance for ${users[i].user} is ${users[i].balance}`
+          );
+          return users[i].balance;
+        }
+      }
+    }
+  } else {
+    // If user not found, show message -> 'User does not exist.'
+    console.log("User does not exist.");
+  }
 }
