39e9d46c266ea31452a6feef4a8f70f9eff6e483
elijaharc <eli.arcedera@gmail.com>
2021-02-09 02:20:23 +0800

    added alerts and displays 9 results

--- Final Frontend Project/app.js
+++ Final Frontend Project/app.js
@@ -7,16 +7,18 @@
 // [] search random photos of food item (for inspiration)
 
 // Variables:
-const recipeDisplay = document.getElementById("content");
+let recipeDisplay = document.getElementById("contentArea");
 let searchInput = document.getElementById("searchInput");
 // const searchQuery = searchInput.value;
-const searchButton = document.getElementById("searchButton");
+let searchButton = document.getElementById("searchButton");
+let cardBody = document.getElementById("card-body");
+let main = document.getElementById("main");
+let noResult = document.getElementById("noResults");
 // .addEventListener("click", searchRequest());
-const content = document.getElementById("content");
 
 // async function for fetching data from API:
 async function searchRequest() {
-  let query = searchInput.value;
+  let query = searchInput.value.trim();
   let APP_ID = "9716ae0a";
   let API_KEY = "03ee4f1c63bc754c18d193783e331e09";
   let response = await fetch(
@@ -29,17 +31,26 @@ async function searchRequest() {
 }
 
 function useApiData(data) {
-  content.innerHTML = `<div class="card" style="width: 18rem;">
-  <img src="${data.hits[0].recipe.image}" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">${data.hits[0].recipe.label}</h5>
-    <p class="card-text">Source: ${data.hits[0].recipe.source}</p>
-    <p class="card-text">Health labels: ${data.hits[0].recipe.healthLabels.join(
-      ", "
-    )}</p>
-    <a href="${
-      data.hits[0].recipe.url
-    }" target="_blank" class="btn btn-primary">View Recipe</a>
-  </div>
-</div>`;
+  if (data.count > 0) {
+    for (let i = 0; i < 9; i++) {
+      let content = document.createElement("div");
+      content.className = "results";
+      content.innerHTML = `<div class="card" style="width: 18rem;">
+        <img src="${data.hits[i].recipe.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${data.hits[i].recipe.label}</h5>
+          <p class="card-text">Source: ${data.hits[i].recipe.source}</p>
+          <p class="card-text">Health labels: ${data.hits[
+            i
+          ].recipe.healthLabels.join(", ")}</p>
+          <a href="${
+            data.hits[i].recipe.url
+          }" target="_blank" class="btn btn-primary">View Recipe</a>
+        </div>
+      </div>`;
+      recipeDisplay.appendChild(content);
+    }
+  } else {
+    noResult.classList.remove("inactive");
+  }
 }
