d57b8847836f68814d31bef1de8e90cad77dbbb0
elijaharc <eli.arcedera@gmail.com>
2021-02-11 04:20:45 +0800

    added duplicate finder

--- Final Frontend Project/app.js
+++ Final Frontend Project/app.js
@@ -1,15 +1,18 @@
-// Main functions
+// Main functions checklist:
 // [x] set up landing page design
 // [x] set up API results display design
 // [x] recipe searching API
 // [x] responsive design
+// [] make title bold font weight
+// [] add remove from favorites button
+// [] add item already added to favorites
 // [] make favorites tab
 // [] toggle list view
 // [] add search in navbar
 // [] add button to bring to top
 // [] organize results in bootstrap grid
 // [] daily/weekly calendar to organize meal plan
-// [] search random photos of food item (for inspiration)
+// [] save favorites in local storage
 
 // Variables:
 let recipeDisplay = document.getElementById("recipeDisplay");
@@ -21,11 +24,12 @@ let noResult = document.getElementById("noResults");
 let addToFave = document.getElementById("addToFave");
 let resultHeader = document.getElementById("resultHeader");
 let favoritesDisplay = document.getElementById("favoritesDisplay");
+let hasSearchDupe = false;
+let hasFaveDupe = false;
 let searchHistory = [];
 let favoritesArray = [];
-let divNum;
 
-// User Constructor
+// Object constructor
 class SearchObject {
   constructor(data) {
     this.data = data;
@@ -35,29 +39,30 @@ class SearchObject {
 function addToFavorites(clickedId) {
   let id = clickedId;
   let index = parseInt(id.replace(/\D/g, ""));
-  favoritesArray.push(searchHistory[0].data.hits[index]);
-  console.log(index);
+  favoritesArray.push(searchHistory[searchHistory.length - 1].data.hits[index]);
+
+  faveDupe();
+  if (hasFaveDupe) {
+    favoritesArray.pop();
+  }
+  hasFaveDupe = false;
+
   console.log(favoritesArray);
-  for (let i = 0; i < favoritesArray.length; i++) {
-    let items = document.createElement("div");
+
+  let items = document.createElement("div");
+  for (let faves of favoritesArray) {
     items.innerHTML = `<div class="card bg-light" style="width: 18rem;">
-    <img src="${
-      searchHistory[0].data.hits[index].recipe.image
-    }" class="card-img-top" alt="Image of ${searchHistory[0].data.q}">
+    <img src="${faves.recipe.image}" class="card-img-top" alt="">
     <div class="card-body">
-      <h5 class="card-title">${
-        searchHistory[0].data.hits[index].recipe.label
-      }</h5>
-      <p class="card-text">Source: ${
-        searchHistory[0].data.hits[index].recipe.source
-      }</p>
-      <p class="card-text">Health Labels: ${searchHistory[0].data.hits[
-        index
-      ].recipe.healthLabels.join(", ")}</p>
+      <h5 class="card-title">${faves.recipe.label}</h5>
+      <p class="card-text">Source: ${faves.recipe.source}</p>
+      <p class="card-text">Health Labels: ${faves.recipe.healthLabels.join(
+        ", "
+      )}</p>
       
       <div class="btn-group btn-group-toggle" data-toggle="buttons">
       <label class="btn btn-sm btn-success active" onClick="window.open('${
-        searchHistory[0].data.hits[index].recipe.url
+        faves.recipe.url
       }', '_blank')">
       <input type="radio" name="options" autocomplete="off" checked> View Recipe
     </label>`;
@@ -71,7 +76,7 @@ async function searchRequest() {
   let APP_ID = "9716ae0a";
   let API_KEY = "03ee4f1c63bc754c18d193783e331e09";
   let response = await fetch(
-    `https://api.edamam.com/search?app_id=${APP_ID}&app_key=${API_KEY}&q=${query}&to=16`
+    `https://api.edamam.com/search?app_id=${APP_ID}&app_key=${API_KEY}&q=${query}&to=20`
   );
   console.log(response);
   let data = await response.json();
@@ -79,14 +84,43 @@ async function searchRequest() {
   useApiData(data);
   let currentSearch = new SearchObject(data);
   searchHistory.push(currentSearch);
+  searchDupe();
+  if (hasSearchDupe) {
+    searchHistory.pop();
+  }
+  hasSearchDupe = false;
   console.log(searchHistory);
+}
+
+// find duplicates in searchHistory array
+function searchDupe() {
+  searchHistory
+    .map((v) => v.data.q)
+    .sort()
+    .sort((a, b) => {
+      if (a === b) hasSearchDupe = true;
+    });
+  // console.log(`Is there a duplicate? ${hasSearchDupe}`);
+  return hasSearchDupe;
+}
+
+// find duplicates in favoritesArray
+function faveDupe() {
+  favoritesArray
+    .map((v) => v.recipe.label)
+    .sort()
+    .sort((a, b) => {
+      if (a === b) hasFaveDupe = true;
+    });
+  console.log(`Is there a duplicate? ${hasFaveDupe}`);
+  return hasFaveDupe;
 }
 
 function useApiData(data) {
   recipeDisplay.innerHTML = "";
   resultHeader.classList.remove("inactive");
   if (data.count > 0) {
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < 20; i++) {
       let content = document.createElement("div");
       content.className = "results";
       content.id = `${data.q}-${i}`;
