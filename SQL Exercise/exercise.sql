881c9604a6c453010f915ccf0eb6acfd154e09e0
elijaharc <eli.arcedera@gmail.com>
2021-03-10 20:05:13 +0800

    joins activity

--- SQL Exercise/exercise.sql
+++ SQL Exercise/exercise.sql
@@ -29,6 +29,11 @@ WHERE id = 1
 -- delete last entry
 DELETE FROM students WHERE id = 6
 
+
+
+
+-- ACTIVITY 2
+
 -- display all students
 SELECT COUNT(*)
 FROM students
@@ -37,12 +42,47 @@ FROM students
 SELECT * FROM students 
 WHERE location = 'Manila';
 
-
 -- display the average age of all students
 SELECT AVG(age) avg_age FROM students;
 
-
 -- display all students by age descending
 SELECT * FROM students
-ORDER BY age DESC; 
+ORDER BY age DESC;
 
+
+
+
+-- JOINS ACTIVITY
+
+-- create another table
+CREATE TABLE classrooms (
+    id int,
+    student_id int,
+    section varchar(1)
+);
+
+-- insert values into table
+INSERT INTO classrooms (id, student_id, section) 
+VALUES  (1, 1, "A"), 
+        (2, 2, "A"),
+        (3, 3, "B"),
+        (4, 4, "C"),
+        (5, 5, "B"),
+        (6, 6, "A"),
+        (7, 7, "C"),
+        (8, 8, "B"),
+        (9, 9, "B"),
+        (10, 10, "C");
+
+-- inner join
+SELECT *
+FROM students as s
+INNER JOIN classrooms as c
+ON s.id = c.id
+ORDER BY s.id ASC;
+
+-- left join
+SELECT *
+FROM classrooms as c
+LEFT JOIN students as s
+ON c.id = s.id;
