c34cce014c2a21efcfab18e02d8e76a14c5a1fd6
Elijah Arcedera <eli.arcedera@gmail.com>
2021-03-16 19:24:36 +0800

    solution

---
+++ Ruby Activities/century_from_year.rb
@@ -0,0 +1,12 @@
+def century_from_year(year)
+    if (year % 100 == 0)
+        return year / 100
+    else
+        return (year - (year % 100))/100 + 1
+    end
+end
+
+puts century_from_year(1705)
+puts century_from_year(1900)
+puts century_from_year(1601)
+puts century_from_year(2000)
