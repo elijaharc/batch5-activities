0a7ba7ceaf4fdcd43e152667def5750bda10fcbe
Elijah Arcedera <eli.arcedera@gmail.com>
2021-03-16 19:25:43 +0800

    added one liner version

--- Ruby Activities/century_from_year.rb
+++ Ruby Activities/century_from_year.rb
@@ -10,3 +10,6 @@ puts century_from_year(1705)
 puts century_from_year(1900)
 puts century_from_year(1601)
 puts century_from_year(2000)
+
+# ONE LINER:
+# year % 100 == 0 ? year / 100 : (year - (year % 100)) / 100 + 1
