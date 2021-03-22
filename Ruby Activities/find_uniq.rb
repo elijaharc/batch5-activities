32e49774a9bce3920e5b6ed8c5d3c343bdfb47aa
Elijah Arcedera <eli.arcedera@gmail.com>
2021-03-22 19:44:31 +0800

    added solutions

--- Ruby Activities/find_uniq.rb
+++ Ruby Activities/find_uniq.rb
@@ -6,5 +6,12 @@ end
 arr1 = [1, 1, 1, 2, 1, 1]
 arr2 = [0, 0, 0.55, 0, 0]
 
+
+
+# other solution
+# def find_uniq(array)
+#     array.map { |e| if array.count(e) == 1; puts e end}
+# end
+
 puts find_uniq(arr1)
 puts find_uniq(arr2)
